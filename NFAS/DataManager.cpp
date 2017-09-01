69ea5396eecd719cd29cf70330a3ce3e90b8ec01
LeiXiaoHang <xiaohangl@sina.cn>
2017-09-01 20:12:10 +0800

    根据设计的数据管理类种的所有信息类，建立sqlserver数据库

---
+++ NFAS/DataManager.cpp
@@ -0,0 +1,592 @@
+#include "DataManager.h"
+#include <QtSql/QSqlDatabase>  
+#include <QMessageBox>
+#include <QTextCodec> 
+#include <QtSql/QSqlError>
+#include <QSqlQuery>  
+#include <QVariant>
+#include <QFile> 
+#include <QTextStream> 
+#include <QSettings>
+
+Academic::Academic()
+{
+
+}
+
+//Academic::Academic(QString id, QString name, QString aclass) :
+//	academicid(id),
+//	academicname(name)
+//{
+//	academicclass = aclass.split(",");
+//}
+
+Academic::Academic(QString id, QString name):
+	academicid(id),
+	academicname(name)
+{
+
+}
+
+Academic::~Academic()
+{
+
+}
+
+//Academic::Academic(const Academic& a)
+//{
+//	academicid = a.academicid;
+//	academicname = a.academicname;
+//	academicclass = a.academicclass;
+//}
+
+Aclass::Aclass()
+{
+
+}
+
+Aclass::Aclass(QString id, QString name, QString academic):
+	classid(id),
+	classname(name),
+	classacademic(academic)
+{
+}
+
+//Aclass::Aclass(QString id, QString name, QString academic, QString student) :
+//	classid(id),
+//	classname(name),
+//	classacademic(academic)
+//{
+//	classstudent = student.split(",");
+//}
+
+//Aclass::Aclass(const Aclass& a)
+//{
+//	classid = a.classid;
+//	classname = a.classname;
+//	classstudent = a.classstudent;
+//}
+
+Student::Student()
+{
+
+}
+
+Student::Student(QString id, QString name, int sex, QString academic, 
+	QString sclass,QString figure, int in, int le, int ab, int to):
+	studentid(id),
+	studentname(name),
+	studentsex(sex), 
+	studentclass(sclass),
+	studentacademic(academic),
+	studentfigure(figure),
+	intimes(in),
+	letimes(le),
+	abtimes(ab),
+	totimes(to)
+{
+
+}
+
+User::User()
+{
+
+}
+
+User::User(QString id, QString psd, int identify):
+	userid(id),
+	password(psd),
+	useridentify(identify)
+{
+
+}
+
+Result::Result()
+{
+
+}
+
+Result::Result(QString ri,QString id, int theory, int actual, QString absencs, QString leave, QString submit):
+	resultid(ri),
+	attendanceid(id),
+	theorynumber(theory),
+	actualnumber(actual),
+	absencestudent(absencs),
+	leavestudent(leave),
+	submituser(submit)
+{
+
+}
+
+Attendance::Attendance()
+{
+
+}
+
+Attendance::Attendance(QString id, QString mid, QString name, QString time, QString aclass):
+	attendanceid(id),
+	attendancemachineid(mid),
+	attendancename(name),
+	attendanceclass(aclass)
+{
+	auto times = time.split("-");
+	if (times.length() == 2) {
+		attendancestarttime = QDateTime::fromString(times[0], "yyyy/MM/dd HH:mm");
+		attendanceendtime = QDateTime::fromString(times[1], "yyyy/MM/dd HH:mm");
+	}
+}
+
+Attendance::Attendance(QString id, QString mid, QString name, QDateTime stime, QDateTime etime, QString aclass):
+	attendanceid(id),
+	attendancemachineid(mid),
+	attendancename(name),
+	attendancestarttime(stime),
+	attendanceendtime(etime),
+	attendanceclass(aclass)
+{
+}
+
+
+const QString Attendance::GetSETime() const
+{
+	return attendancestarttime.toString("yyyy/MM/dd HH:mm") + "-" + attendanceendtime.toString("yyyy/MM/dd HH:mm");
+}
+
+
+DataManager::DataManager(QString configfile)//从配置文件中读取数据库连接信息
+{
+	QString server = "127.0.0.1";//默认连接信息
+	QString database = "nfas";
+	QString uid = "sa";
+	QString pwd = "586926";
+
+	//配置文件操作类
+	QSettings *configIni = new QSettings(configfile, QSettings::IniFormat);
+	
+	
+	
+	QFile config(configfile);//判断文件是否存在
+	if (!config.exists())//如果文件不存在.自动生成
+	{
+	/*	config.open(QIODevice::WriteOnly| QIODevice::Text);
+		QTextStream configout(&config);
+		configout << "Server:" << server << endl;
+		configout << "DataBase:" << database << endl;
+		configout << "UserID:" << uid << endl;
+		configout << "Password:" << pwd << endl;
+		config.close();*/
+
+		//----------------	
+		configIni->setValue("DBServer/ip", server);
+		configIni->setValue("/DBServer/database", database);
+		configIni->setValue("/DBServer/uid", uid);
+		configIni->setValue("/DBServer/pwd", pwd);
+	}
+	else//读取配置文件
+	{
+		//--------------		
+		server		=	configIni->value("/DBServer/ip").toString();
+		database	=	configIni->value("/DBServer/database").toString();
+		uid			=	configIni->value("/DBServer/uid").toString();
+		pwd			=	configIni->value("/DBServer/pwd").toString();
+	}
+
+	//新建数据类对象保存数据
+
+	//学院、学生、班级、考勤、用户、考勤结果
+	academics = new QVector<Academic>;
+	students = new QVector<Student>;
+	classes = new QVector<Aclass>;
+	attendances = new QVector<Attendance>;
+	users = new QVector<User>;
+	results = new QVector<Result>;
+
+	if (!InitDataBase(server, database, uid, pwd)) //数据库连接
+	{
+		exit(-1);
+	}
+	delete configIni;
+}
+
+//数据库连接
+bool DataManager::InitDataBase(QString server,QString database,QString uid,QString pwd)
+{
+	QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+	db.setDatabaseName(QString("DRIVER={SQL SERVER};"
+		"SERVER=%1;"
+		"DATABASE=%2;"
+		"UID=%3;"
+		"PWD=%4;").arg(server)
+		.arg(database)
+		.arg(uid)
+		.arg(pwd));
+	if (!db.open())
+	{
+		//数据连接失败，并给出sqlserver的错误提示
+		QMessageBox::warning(0, tr("Cannot open database"),
+			db.lastError().databaseText(), QMessageBox::Cancel);
+		return false;
+	}
+	//初始化各项数据列表
+	//首先从数据库中查询出各项信息，加载到程序中
+	InitAcademics();
+	InitStudents();
+	InitClasss();
+	InitAttendances();
+	//InitResult();
+	InitUser();
+	return true;
+}
+
+bool DataManager::AcademicOP(const Academic& a, int op)
+{
+	QSqlQuery query;
+	switch (op)
+	{
+	case 0:
+		query.prepare("INSERT INTO academictable (academicid,academicname) VALUES (?,?)");
+		query.bindValue(0, a.GetID());
+		query.bindValue(1, a.GetName());
+		break;
+	case 1:
+		query.prepare("DELETE FROM academictable WHERE academicid=?");
+		query.bindValue(0, a.GetID());
+		break;
+	default:
+		break;
+	}
+	return query.exec();
+}
+
+bool DataManager::AclassOP(const Aclass& a, int op)
+{
+	QSqlQuery query1;
+	switch (op)
+	{
+	case 0:
+		query1.prepare("INSERT INTO classtable (classid,classname,classacademic) VALUES (?,?,?)");
+		query1.bindValue(0, a.GetID());
+		query1.bindValue(1, a.GetName());
+		query1.bindValue(2, a.GetAcademic());
+		break;
+	case 1:
+		query1.prepare("UPDATE classtable SET classname=?,classacademic=? WHERE classid=?");
+		query1.bindValue(0, a.GetName());
+		query1.bindValue(1, a.GetAcademic());
+		query1.bindValue(2, a.GetID());
+		break;
+	case 2:
+		query1.prepare("DELETE FROM classtable WHERE classid=?");
+		query1.bindValue(0, a.GetID());
+		break;
+	default:
+		break;
+	}
+	return query1.exec();
+}
+
+bool DataManager::StudentOP(const Student& s, int op)
+{
+	QSqlQuery query1;
+	switch (op)
+	{
+	case 0:
+		query1.prepare("INSERT INTO studenttable (studentid,studentname,studentsex,studentacademic,studentclass,studentfigure) VALUES (?,?,?,?,?,?)");
+		query1.bindValue(0, s.GetID());
+		query1.bindValue(1, s.GetName());
+		query1.bindValue(2, s.GetSex());
+		query1.bindValue(3, s.GetAcademic());
+		query1.bindValue(4, s.GetAclass());
+		query1.bindValue(5, s.GetFigure());
+		break;
+	case 1:
+		query1.prepare("UPDATE studenttable SET studentname=?,studentsex=?,studentacademic=?,studentclass=? WHERE studentid=?");
+		query1.bindValue(0, s.GetName());
+		query1.bindValue(1, s.GetSex());
+		query1.bindValue(2, s.GetAcademic());
+		query1.bindValue(3, s.GetAclass());
+		query1.bindValue(4, s.GetID());
+		break;
+	case 2:
+		query1.prepare("DELETE FROM studenttable WHERE studentid=?");
+		query1.bindValue(0, s.GetID());
+		break;
+	case 3:
+		query1.prepare("UPDATE studenttable SET intimes=?,totimes=? WHERE studentid=?");
+		query1.bindValue(0, s.GetIntimes());
+		query1.bindValue(1, s.GetTotimes());
+		query1.bindValue(2, s.GetID());
+		break;
+	case 4:
+		query1.prepare("UPDATE studenttable SET letimes=?,totimes=? WHERE studentid=?");
+		query1.bindValue(0, s.GetLetimes());
+		query1.bindValue(1, s.GetTotimes());
+		query1.bindValue(2, s.GetID());
+		break;
+	case 5:
+		query1.prepare("UPDATE studenttable SET abtimes=?,totimes=? WHERE studentid=?");
+		query1.bindValue(0, s.GetAbtimes());
+		query1.bindValue(1, s.GetTotimes());
+		query1.bindValue(2, s.GetID());
+		break;
+	case 6:
+		query1.prepare("UPDATE studenttable SET intimes=?,letimes=?,abtimes=?,totimes=? WHERE studentid=?");
+		query1.bindValue(0, s.GetIntimes());
+		query1.bindValue(1, s.GetLetimes());
+		query1.bindValue(2, s.GetAbtimes());
+		query1.bindValue(3, s.GetTotimes());
+		query1.bindValue(4, s.GetID());
+		break;
+	default:
+		break;
+	}
+	return query1.exec();
+}
+
+bool DataManager::UserOP(const User& u, int op)
+{
+	QSqlQuery query;
+	switch (op)
+	{
+	case 0:
+		query.prepare("INSERT INTO admintable (userid,userpassword,useridentify) VALUES (?,?,?)");
+		query.bindValue(0, u.GetUID());
+		query.bindValue(1, u.GetPsd());
+		query.bindValue(2, u.GetIdentify());
+		break;
+	case 1:
+		query.prepare("UPDATE admintable SET useridentify=? WHERE userid=?");
+		query.bindValue(0, u.GetIdentify());
+		query.bindValue(1, u.GetUID());
+		break;
+	case 2:
+		query.prepare("DELETE FROM admintable WHERE userid=?");
+		query.bindValue(0, u.GetUID());
+		break;
+	default:
+		break;
+	}
+	return query.exec();
+}
+
+bool DataManager::AttendanceOP(const Attendance& a, int op)
+{
+	QSqlQuery query;
+	switch (op)
+	{
+	case 0:
+		query.prepare("INSERT INTO attendancetable (attendanceid,attendancemachineid,attendancename,attendancetime,attendanceclass) VALUES (?,?,?,?,?)");
+		query.bindValue(0, a.GetID());
+		query.bindValue(1, a.GetMID());
+		query.bindValue(2, a.GetName());
+		query.bindValue(3, a.GetSETime());
+		query.bindValue(4, a.GetAclass());
+		break;
+	case 1:
+		query.prepare("UPDATE attendancetable SET attendanceclass=? WHERE attendanceid=?");
+		query.bindValue(0, a.GetAclass());
+		query.bindValue(1, a.GetID());
+		break;
+	case 2:
+		query.prepare("DELETE FROM attendancetable WHERE attendanceid=?");
+		query.bindValue(0, a.GetID());
+		break;
+	default:
+		break;
+	}
+	return query.exec() /*&& ResultOP(Result(a.GetRID(), 0, 0,"","",""), 2)*/;
+}
+
+bool DataManager::ResultOP(const Result& a, int op)
+{
+	QSqlQuery query;
+	switch (op)
+	{
+	case 0:
+		query.prepare("INSERT INTO resulttable (resultid,attendanceid,actualnumber,theorynumber,absencestudent,leavestudent,submituser) VALUES (?,?,?,?,?,?,?)");
+		query.bindValue(0, a.GetRID());
+		query.bindValue(1, a.GetAID());
+		query.bindValue(2, a.GetANum());
+		query.bindValue(3, a.GetTNum());
+		query.bindValue(4, a.GetAStu());
+		query.bindValue(5, a.GetLStu());
+		query.bindValue(6, a.GetUID());
+		break;
+	case 1:
+		query.prepare("UPDATE resulttable SET theorynumber=?,actualnumber=?,absencestudent=?,leavestudent=? WHERE resultid=? AND attendanceid=?");
+		query.bindValue(0, a.GetTNum());
+		query.bindValue(1, a.GetANum());
+		query.bindValue(2, a.GetAStu());
+		query.bindValue(3, a.GetLStu());
+		query.bindValue(4, a.GetRID());
+		query.bindValue(5, a.GetAID());
+		break;
+	case 2:
+		if (a.GetRID() == "") {
+			query.prepare("DELETE FROM resulttable WHERE attendanceid=?");
+			query.bindValue(0, a.GetAID());
+		}
+		else {
+			query.prepare("DELETE FROM resulttable WHERE resultid=?");
+			query.bindValue(0, a.GetRID());
+		}
+		break;
+	default:
+		break;
+	}
+	return query.exec();
+}
+
+void DataManager::updateAcademic()
+{
+	academics->clear();
+	InitAcademics();
+}
+
+void DataManager::updateClass()
+{
+	classes->clear();
+	InitClasss();
+}
+
+void DataManager::updateStudent()
+{
+	students->clear();
+	InitStudents();
+}
+
+void DataManager::updateAttendance()
+{
+	attendances->clear();
+	InitAttendances();
+}
+
+void DataManager::updateResult(User user)
+{
+	results->clear();
+	InitResult(user);
+}
+
+void DataManager::updateUser()
+{
+	users->clear();
+	InitUser();
+}
+
+QList<Aclass> DataManager::GetClassFromString(QStringList classes)
+{
+	return QList<Aclass>();
+}
+
+QList<Student> DataManager::GetStudentFromString(QStringList students)
+{
+	return QList<Student>();
+}
+
+void DataManager::InitAcademics()
+{
+	QSqlQuery query("SELECT * FROM academictable");
+	while (query.next()) {
+		QString id = query.value(0).toString();
+		QString name = query.value(1).toString();
+		//QString aclass = query.value(2).toString();
+		this->academics->append(Academic(id, name/*, aclass*/));
+	}
+}
+
+void DataManager::InitStudents()
+{
+	QSqlQuery query("SELECT * FROM studenttable");
+	while (query.next()) {
+		QString id = query.value(0).toString();
+		QString name = query.value(1).toString();
+		int sex = query.value(2).toInt();
+		QString academic = query.value(4).toString();
+		QString aclass = query.value(3).toString();
+		QString figure = query.value(5).toString();
+		int in = query.value(6).toInt();
+		int le = query.value(7).toInt();
+		int ab = query.value(8).toInt();
+		int to = query.value(9).toInt();
+		this->students->append(Student(id, name,sex,academic,aclass,figure,in,le,ab,to));
+	}
+}
+
+void DataManager::InitClasss()
+{
+	QSqlQuery query("SELECT * FROM classtable");
+	while (query.next()) 
+	{
+		QString id = query.value(0).toString();
+		QString name = query.value(1).toString();
+		QString student = query.value(2).toString();
+		classes->append(Aclass(id, name, student));
+	}
+}
+
+void DataManager::InitResult(User user)
+{
+	QSqlQuery query("SELECT * FROM resulttable");
+	while (query.next()) 
+	{
+		QString rid = query.value(0).toString();
+		QString id = query.value(1).toString();
+		int tnum = query.value(2).toInt();
+		int anum = query.value(3).toInt();
+		QString astu = query.value(4).toString();
+		QString lstu = query.value(5).toString();
+		QString uid = query.value(6).toString();
+
+		//如果权限为 0管理员的话，将所有的考勤结果都添加到程序中
+		if (user.GetIdentify() == 0)
+		{
+			results->append(Result(rid,id, tnum, anum, astu, lstu, uid));
+		}
+		else//如果为 1 教师的话，只是将自己提交的结果表添加到结果信息中
+		{
+			if (uid == user.GetUID())
+			{
+				results->append(Result(rid,id, tnum, anum, astu, lstu, uid));
+			}
+		}
+	}
+}
+
+void DataManager::InitUser()
+{
+	QSqlQuery query("SELECT * FROM admintable");
+	while (query.next()) {
+		QString id = query.value(0).toString();
+		int identify = query.value(2).toInt();
+		users->append(User(id, "", identify));
+	}
+}
+
+bool DataManager::UserLogin(User user)
+{
+	QSqlQuery query(QString("SELECT * FROM admintable WHERE userid='%1' AND userpassword='%2'")
+		.arg(user.GetUID())
+		.arg(user.GetPsd()));
+	//如果匹配到该用户，则设置为当前操作用户
+	while (query.next())
+	{
+		currentusers.SetUID(query.value(0).toString());
+		currentusers.SetIdentify(query.value(2).toInt());
+		return true;
+	}
+	return false;
+}
+
+void DataManager::InitAttendances()
+{
+	QSqlQuery query("SELECT * FROM attendancetable");
+	while (query.next()) {
+		QString id = query.value(0).toString();
+		QString mid = query.value(1).toString();
+		QString name = query.value(2).toString();
+		QString time = query.value(3).toString();
+		QString aclass = query.value(4).toString();
+		attendances->append(Attendance(id, mid, name, time, aclass));
+	}
+}
+
---
+++ NFAS/DataManager.h
@@ -0,0 +1,234 @@
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+#include <QString>
+#include <QStringList>
+#include <QDateTime>
+#include <QObject>
+#include <QVector>
+
+
+//学生类
+class Student{
+
+public:
+	Student();
+	Student(QString id, QString name, int sex, QString academic, QString sclass, QString figure,int in=0,int le=0,int ab=0,int to=0);
+
+	const QString GetID() const{ return studentid; }
+	const QString GetName() const{ return studentname; }
+	const QString GetAcademic() const{ return studentacademic; }
+	const QString GetAclass() const{ return studentclass; }
+	const int GetSex() const{ return studentsex; }
+	const QString GetFigure() const{ return studentfigure; }
+	const int GetIntimes() const { return intimes; }
+	const int GetLetimes() const { return letimes; }
+	const int GetAbtimes() const { return abtimes; }
+	const int GetTotimes() const { return totimes; }
+	void AddIntime() { ++intimes; }
+	void AddLetime() { ++letimes; }
+	void AddAbtime() { ++abtimes; }
+	void AddTotime() { ++totimes; }
+	void MinusIntime() { --intimes; }
+	void MinusLetime() { --letimes; }
+	void MinusAbtime() { --abtimes; }
+	void MinusTotime() { --totimes; }
+private:
+	QString studentid;
+	QString studentname;
+	int studentsex;
+	QString studentclass;
+	QString studentacademic;
+	QString studentfigure;
+	int intimes;
+	int letimes;
+	int abtimes;
+	int totimes;
+};
+
+//班级类
+class Aclass {
+
+public:
+	Aclass();
+	//Aclass(const Aclass& a);
+	Aclass(QString id, QString name,QString academic);
+	//Aclass(QString id, QString name, QString academic, QString student);
+
+	const QString GetID() const { return classid; }
+	const QString GetName() const { return classname; }
+	const QString GetAcademic() const { return classacademic; }
+
+private:
+	QString classid;
+	QString classname;
+	QString classacademic;
+	//QStringList classstudent;
+};
+
+//学院类
+class Academic{
+
+public:
+	Academic();
+	//Academic(const Academic& a);
+	Academic(QString id, QString name);
+	//Academic(QString id, QString name, QString aclass);
+
+	~Academic();
+
+	const QString GetID() const{ return academicid; }
+	const QString GetName() const{ return academicname; }
+
+private:
+	QString academicid;
+	QString academicname;
+	//QStringList academicclass;
+};
+
+
+//用户类
+class User {
+
+public:
+	User();
+	User(QString id, QString psd, int identify);
+
+	const QString GetUID() const{ return userid; }
+	const QString GetPsd() const{ return password; }
+	const int GetIdentify() const{ return useridentify; }
+
+	void SetUID(QString id) { userid = id; }
+	void SetIdentify(int identify) { useridentify = identify; }
+private:
+	QString userid;
+	QString password;
+	int useridentify;
+};
+
+
+//考勤结果类
+class Result {
+
+public:
+	Result();
+	Result(QString rid,QString id, int theory, int actual, QString absencs,QString leave,QString submit);
+
+	void SetRID(QString ri) { resultid = ri; }
+	void SetAID(QString id) { attendanceid = id; }
+	void SetTNum(int num) { theorynumber = num; }
+	void SetANum(int num) { actualnumber = num; }
+	void SetAStu(QString a) { absencestudent = a; }
+	void SetLStu(QString a) { leavestudent = a; }
+	void SetUID(QString a) { submituser = a; }
+
+	const QString GetRID() const { return resultid; }
+	const QString GetAID() const { return attendanceid; }
+	const int GetTNum() const { return theorynumber; }
+	const int GetANum() const { return actualnumber; }
+	const QString GetAStu() const { return absencestudent; }
+	const QString GetLStu() const { return leavestudent; }
+	const QString GetUID() const { return submituser; }
+
+private:
+	QString resultid;
+	QString attendanceid;
+	int theorynumber;
+	int actualnumber;
+	QString absencestudent;
+	QString leavestudent;
+	QString submituser;
+};
+
+
+//考勤信息类
+class Attendance {
+
+public:
+	Attendance();
+	Attendance(QString id, QString mid, QString name, QString time, QString aclass);
+	Attendance(QString id, QString mid, QString name, QDateTime stime, QDateTime etime, QString aclass);
+
+	//QString TimeToString();
+	//QString StudentToString();
+
+	const QString GetID() const { return attendanceid; }
+	const QString GetName() const { return attendancename; }
+	const QString GetMID() const { return attendancemachineid; }
+	const QString GetAclass() const { return attendanceclass; }
+	const QString GetSETime() const;
+private:
+	QString attendanceid;
+	QString attendancemachineid;
+	QString attendancename;
+	QDateTime attendancestarttime;
+	QDateTime attendanceendtime;
+	QString attendanceclass;
+};
+
+
+//数据管理类
+class DataManager:public QObject{
+	Q_OBJECT
+public:
+	explicit DataManager(QString configfile);
+
+	//数据库的更新（插入，删除）,通过op方式来确定，
+
+	//  0：插入
+	//	1:删除
+	bool AcademicOP(const Academic& a, int op);
+	bool AclassOP(const Aclass& a, int op);
+	bool StudentOP(const Student& s, int op);
+	bool UserOP(const User& u, int op);
+	bool AttendanceOP(const Attendance& a, int op);
+	bool ResultOP(const Result& a, int op);
+
+	bool UserLogin(User user);
+
+
+
+	//从数据库中更新最新的数据到程序中
+	void updateAcademic();
+	void updateClass();
+	void updateStudent();
+	void updateAttendance();
+	void updateResult(User user);
+	void updateUser();
+
+	QList<Aclass> GetClassFromString(QStringList classes);
+	QList<Student> GetStudentFromString(QStringList students);
+
+	QVector<Academic>* GetAcademics() { return academics; }
+	QVector<Aclass>* GetAClass() { return classes; }
+	QVector<Student>* GetStudent() { return students; }
+	QVector<Attendance>* GetAttendance() { return attendances; }
+	QVector<Result>* GetResult() { return results; }
+	QVector<User>* GetUser() { return users; }
+	User GetCurrentUser() { return currentusers; }
+
+private:
+
+	//数据库连接
+	bool InitDataBase(QString server, QString database, QString uid, QString pwd);
+
+	//初始化数据信息，
+	//将数据库中的数据更新到程序中
+	void InitAcademics();
+	void InitStudents();
+	void InitClasss();
+	void InitResult(User user);
+	void InitUser();
+	void InitAttendances();
+
+	QVector<Academic>* academics;//学院列表
+	QVector<Aclass>* classes;//班级列表
+	QVector<Student>* students;//学生列表
+	QVector<Attendance>* attendances;//考勤列表
+	QVector<Result>* results;//考勤结果表
+	QVector<User>* users;//用户列表
+	User currentusers;//当前用户
+};
+
+
+#endif // DATAMANAGER_H
