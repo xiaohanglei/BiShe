2c164c632f3fd35ae18b0f050b2583d22017c8d2
XiaoHangLei <xiaohangl@sina.cn>
2018-02-02 20:47:41 +0800

    增加模拟考勤项目的数据通讯部分、数据分帧部分、增加模拟考勤项目的翻译文件，

--- TestSAM/testsam_zh.ts
+++ TestSAM/testsam_zh.ts
@@ -2,6 +2,156 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>Attendance</name>
+    <message>
+        <location filename="attendance.ui" line="16"/>
+        <source>Attendance</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AttendanceM</name>
+    <message>
+        <location filename="attendance.cpp" line="75"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attendance.cpp" line="75"/>
+        <source>Recv Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attendance.cpp" line="107"/>
+        <source>Attendance Machine Simulator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attendance.cpp" line="119"/>
+        <source>StuId</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attendance.cpp" line="123"/>
+        <source>Finger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="attendance.cpp" line="125"/>
+        <source>submit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TcpClient</name>
+    <message>
+        <location filename="tcpclient.cpp" line="55"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="55"/>
+        <source>Init NetWork Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TestSAM</name>
+    <message>
+        <location filename="testsam.cpp" line="25"/>
+        <location filename="testsam.cpp" line="32"/>
+        <location filename="testsam.cpp" line="39"/>
+        <location filename="testsam.cpp" line="46"/>
+        <location filename="testsam.cpp" line="52"/>
+        <location filename="testsam.cpp" line="61"/>
+        <location filename="testsam.cpp" line="64"/>
+        <location filename="testsam.cpp" line="86"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="25"/>
+        <source>Please Select Attendance Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="32"/>
+        <location filename="testsam.cpp" line="39"/>
+        <source>Ip Address Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="46"/>
+        <source>Port Address Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="52"/>
+        <source>Class Room Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="61"/>
+        <source>Server Connection failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="64"/>
+        <source>Server Connection successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="94"/>
+        <source>Attendance Machine Simulator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="119"/>
+        <source>Table</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="120"/>
+        <source>StuId</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="123"/>
+        <source>Server Ip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="124"/>
+        <source>Server Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="125"/>
+        <source>Class Room</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="145"/>
+        <source>Attendance Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="158"/>
+        <source>Environment Setting </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="161"/>
+        <source>Submit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="testsam.cpp" line="164"/>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>TestSAMClass</name>
     <message>
         <location filename="testsam.ui" line="16"/>
